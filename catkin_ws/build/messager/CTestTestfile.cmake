4dac9bcd170aa06abee799a04ebab751a57f3115
fanliyuan <1244969848@qq.com>
2020-11-05 22:43:02 -0800

    first test

---
+++ catkin_ws/build/messager/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/fan/catkin_ws/src/messager
+# Build directory: /home/fan/catkin_ws/build/messager
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
